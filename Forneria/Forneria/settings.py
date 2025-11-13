e50d5a14dba4d84c4721b619288eb2cf8dbf3560
Laboratorio Computación N° 8 <claseslab8@inacap.cl>
2025-11-13 19:21:14 -0300

    Se bajan los modelos requeridos de la base de datos

--- Forneria/Forneria/settings.py
+++ Forneria/Forneria/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Forneria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',   # Cambiamos el motor a MySQL
+        'NAME': 'Forneria',            # Nombre de tu base de datos
+        'USER': 'Alucard',                # Usuario de MySQL
+        'PASSWORD': 'belmont',            # Contraseña del usuario
+        'HOST': 'localhost',                    # Servidor (localhost si es local)
+        'PORT': '3306',                         # Puerto por defecto de MySQL
     }
 }
 
--- Forneria/apps/inventario/models.py
+++ Forneria/apps/inventario/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
+    marca = models.CharField(max_length=100, blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    caducidad = models.DateField()
+    elaboracion = models.DateField(blank=True, null=True)
+    tipo = models.CharField(max_length=100, blank=True, null=True)
+    stock_actual = models.IntegerField(blank=True, null=True)
+    stock_minimo = models.IntegerField(blank=True, null=True)
+    stock_maximo = models.IntegerField(blank=True, null=True)
+    presentacion = models.CharField(max_length=100, blank=True, null=True)
+    formato = models.CharField(max_length=100, blank=True, null=True)
+    creado = models.DateTimeField(blank=True, null=True)
+    modificado = models.DateTimeField(blank=True, null=True)
+    eliminado = models.DateTimeField(blank=True, null=True)
+    categorias = models.ForeignKey('Categorias', models.DO_NOTHING)
+    nutricional = models.ForeignKey('Nutricional', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'productos'
+
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'categorias'
+
+
+class MovimientosInventario(models.Model):
+    tipo_movimiento = models.CharField(max_length=7)
+    cantidad = models.IntegerField()
+    fecha = models.DateTimeField()
+    productos = models.ForeignKey(Productos, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'movimientos_inventario'
