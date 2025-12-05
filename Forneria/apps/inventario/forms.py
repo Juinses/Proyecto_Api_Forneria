167ade60115161cc28ebe1d9e3bca5066477e8b9
Juinses <juandavid.blancocarmona2005@gmail.com>
2025-12-04 21:19:29 -0300

    cambios al estilo

--- Forneria/apps/inventario/forms.py
+++ Forneria/apps/inventario/forms.py
@@ -9,6 +9,22 @@ class ProductoForm(forms.ModelForm):
             'elaboracion', 'tipo', 'stock_actual', 'stock_minimo',
             'stock_maximo', 'presentacion', 'formato', 'categorias', 'nutricional'
         ]
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'marca': forms.TextInput(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+            'caducidad': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'elaboracion': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'tipo': forms.Select(attrs={'class': 'form-select'}),
+            'stock_actual': forms.NumberInput(attrs={'class': 'form-control'}),
+            'stock_minimo': forms.NumberInput(attrs={'class': 'form-control'}),
+            'stock_maximo': forms.NumberInput(attrs={'class': 'form-control'}),
+            'presentacion': forms.TextInput(attrs={'class': 'form-control'}),
+            'formato': forms.Select(attrs={'class': 'form-select'}),
+            'categorias': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'nutricional': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
